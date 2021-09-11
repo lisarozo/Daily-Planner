3fa0abde090c7ae2a181a904895ea175512afc97
lisarozo <erozo190@gmail.com>
2021-09-11 11:45:04 -0500

    Added Local Storage

--- index.js
+++ index.js
@@ -2,13 +2,21 @@ var currentTime = moment().format('MMM Do YYYY')
 var timeEl = document.getElementById("currentDay")
 var currentHour = parseInt(moment().format('HH'))
 // var currentHour = 10
-
+var local9 = localStorage.getItem('9AM')
+console.log(local9)
 timeEl.textContent = currentTime;
 // console.log(currentTime, currentHour)
+var textarea = document.getElementById(9)
+textarea.value = local9;
+// textarea.addEventListener('input', updateValue);
 
+function updateValue(e) {
+  var textContent = e.target.value;
+  console.log(textContent)} 
 for(i=9; i<=17; i++){
     var textareaEl = document.getElementById(i)
-
+    if(textareaEl != null ){
+        
     if(i<currentHour){
         textareaEl.setAttribute("class", "col-8 past")
     }else if(i == currentHour) {
@@ -16,13 +24,24 @@ for(i=9; i<=17; i++){
     }else{
         textareaEl.setAttribute("class", "col-8 future")
     }
-
+    
     var buttonEl = document.getElementById("btn-" + i)
     buttonEl.addEventListener("click", saveToLocalStorage)
     console.log(textareaEl)
+}
 }
 
 function saveToLocalStorage(event) {
     event.preventDefault()
     console.log(event.target)
+    // localStorage.setItem('9AM', textareaEl.value);
+    // localStorage.setItem('10AM', textarea.value);
+    // localStorage.setItem('11AM', textarea.value);
+    // localStorage.setItem('12PM', textarea.value);
+    // localStorage.setItem('13PM', textarea.value);
+    // localStorage.setItem('14PM', textarea.value);
+    // localStorage.setItem('15PM', textarea.value);
+    // localStorage.setItem('16PM', textarea.value);
+    // localStorage.setItem('17PM', textarea.value);
+    // console.log(textarea.value)
 }
